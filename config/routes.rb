67ef7a141757ea44cc2bf663dfbb4dc5ae5b1b8c
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-04-02 15:47:24 -0400

    Slight update to layout for easy navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   root "application#home"
 
-  get "/albums/list" => "albums#list"
-  get "/albums/concerts" => "albums#concert"
+  get "/albums/list" => "albums#list", as: "albums_list"
+  get "/albums/concerts" => "albums#concert", as: "concerts"
 
   post "/albums/:album_id/favorite" => "albums#favorite", as: "favorite_album"
   delete "albums/:album_id/unfavorite" => "albums#unfavorite", as: "unfavorite_album"
