91fe4eb6198b8e664dbe159a608dcff9808f431f
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-04-09 22:55:22 -0400

    Starting to wrap

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,3 +3,16 @@ $.ajaxSetup( {
     xhr.setRequestHeader( 'X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
   }
 });
+
+//Scroll Animation for About
+$(function() {
+    //caches a jQuery object containing the header element
+    var animate = $("#progress");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500) {
+            animate.removeClass('display-none').addClass("display");
+        } 
+    });
+});
